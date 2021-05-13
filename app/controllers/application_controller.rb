56d6122ea59304d7668c4a1893622b056393f75d
Yola21 <yashkhorja4@gmail.com>
2021-05-13 23:56:11 +0530

    Added pundit task policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :handle_unauthorized_user
+
 
   def authenticate_user_using_x_auth_token
     user_email = request.headers["X-Auth-Email"]
@@ -22,4 +25,8 @@ class ApplicationController < ActionController::Base
     def current_user
       @current_user
     end
+
+  def handle_unauthorized_user
+    render json: { error: "Permission Denied" }, status: :forbidden
+  end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,12 +3,13 @@ class TasksController < ApplicationController
   before_action :load_task, only: %i[show update destroy]
 
   def index
-    tasks = Task.all
+    tasks = policy_scope(Task)
     render status: :ok, json: { tasks: tasks }
   end
 
   def create
     @task = Task.new(task_params.merge(creator_id: @current_user.id))
+    authorize @task
     if @task.save
       render status: :ok, json: { notice: t('successfully_created', entity: 'Task') }
     else
@@ -20,12 +21,20 @@ class TasksController < ApplicationController
   end
 
   def show
+    authorize @task
     task_creator = User.find(@task.creator_id).name
     render status: :ok, json: { task: @task, assigned_user: @task.user, task_creator: task_creator }
   end
 
   def update
-    if @task.update(task_params)
+    authorize @task
+    is_not_owner = @task.creator_id != current_user.id
+
+    if task_params[:authorize_owner] && is_not_owner
+      render status: :forbidden, json: { error: t('authorization.denied') }
+    end
+
+    if @task.update(task_params.except(:authorize_owner))
       render status: :ok, json: { notice: 'Successfully updated task.' }
     else
       render status: :unprocessable_entity, json: { errors: @task.errors.full_messages }
@@ -33,6 +42,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    authorize @task
     if @task.destroy
       render status: :ok, json: { notice: 'Successfully deleted task.' }
     else
@@ -46,12 +56,12 @@ class TasksController < ApplicationController
   def load_task
     @task = Task.find_by_slug!(params[:slug])
     rescue ActiveRecord::RecordNotFound => errors
-      render json: {errors: errors}
+      render json: {errors: errors}, status: :not_found
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :user_id)
+    params.require(:task).permit(:title, :user_id, :authorize_owner)
   end
 end
